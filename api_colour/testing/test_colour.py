435baeef3a4258fa0eadc4c4681646b7a6452cbf
afzal <kotadia.afzal@gmail.com>
2022-07-20 13:51:30 +0000

    testing for post

--- api_colour/testing/test_colour.py
+++ api_colour/testing/test_colour.py
@@ -20,6 +20,47 @@ class TestResponse(TestBase):
 
             response = self.client.get(url_for('get_order'))
             self.assertIn(b'Apples Juice', response.data)
+            self.assertIn(b'Apple Juice would be nice', response.data)
+
+    def test_bananas_juice(self):
+        with requests_mock.Mocker() as m:
+            m.get('http://service2:5001/get/food', text='Bananas')
+            m.get('http://service3:5050/get/drink', text='Juice')
+
+            response = self.client.get(url_for('get_order'))
+            self.assertIn(b'Bananas Juice', response.data)
+            self.assertIn(b'Not sure about Banana Juice', response.data)
+
+    def test_grapes_juice(self):
+        with requests_mock.Mocker() as m:
+            m.get('http://service2:5001/get/food', text='Grapes')
+            m.get('http://service3:5050/get/drink', text='Juice')
+
+            response = self.client.get(url_for('get_order'))
+            self.assertIn(b'Grapes Juice', response.data)
+            self.assertIn(b'Grape Juice sounds good', response.data)
+
+    def test_strawberries_juice(self):
+        with requests_mock.Mocker() as m:
+            m.get('http://service2:5001/get/food', text='Strawberries')
+            m.get('http://service3:5050/get/drink', text='Juice')
+
+            response = self.client.get(url_for('get_order'))
+            self.assertIn(b'Strawberries Juice', response.data)
+            self.assertIn(b'Strawberry juice is an interesting one', response.data)
+
+    def test_oranges_juice(self):
+        with requests_mock.Mocker() as m:
+            m.get('http://service2:5001/get/food', text='Oranges')
+            m.get('http://service3:5050/get/drink', text='Juice')
+
+            response = self.client.get(url_for('get_order'))
+            self.assertIn(b'Oranges Juice', response.data)
+            self.assertIn(b'Cannot go wrong with OJ', response.data)
+   
+
+
+
 
     def test_apples_milkshake(self):
         with requests_mock.Mocker() as m:
@@ -28,6 +69,7 @@ class TestResponse(TestBase):
 
             response = self.client.get(url_for('get_order'))
             self.assertIn(b'Apples Milkshake', response.data)
+            self.assertIn(b'unavailable', response.data)
 
     def test_apples_smoothie(self):
         with requests_mock.Mocker() as m:
@@ -36,6 +78,7 @@ class TestResponse(TestBase):
 
             response = self.client.get(url_for('get_order'))
             self.assertIn(b'Apples Smoothie', response.data)
+            self.assertIn(b'unavailable', response.data)
 
     def test_apples_coffee(self):
         with requests_mock.Mocker() as m:
@@ -44,6 +87,7 @@ class TestResponse(TestBase):
 
             response = self.client.get(url_for('get_order'))
             self.assertIn(b'Apples Coffee', response.data)
+            self.assertIn(b'unavailable', response.data)
 
     def test_bananas_smoothie(self):
         with requests_mock.Mocker() as m:
@@ -52,3 +96,4 @@ class TestResponse(TestBase):
 
             response = self.client.get(url_for('get_order'))
             self.assertIn(b'Bananas Smoothie', response.data)
+            self.assertIn(b'unavailable', response.data)
