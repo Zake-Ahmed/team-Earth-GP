08f2d5452b3c1e8f77116bfbfaea8e78f1bbd442
gregorule <104358226+gregorule@users.noreply.github.com>
2022-07-20 12:16:38 +0100

    Update routes.py

--- api_colour/application/routes.py
+++ api_colour/application/routes.py
@@ -3,8 +3,8 @@ from flask import Flask, Response
 
 @app.route('/', methods=['GET'])
 def get_order():
-    food = (requests.get('http://service1:5001/get/food')).text
-    drink = (requests.get('http://service1:5050/get/drink')).text
+    food = (requests.get('http://service2:5001/get/food')).text
+    drink = (requests.get('http://service3:5050/get/drink')).text
     
     order = food + " " + drink
 
